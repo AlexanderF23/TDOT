4d4a767a69f34cfbd117d1986b22edab33307f3f
Alexander <Alexander.Friedrichsen7@gmail.com>
2025-01-25 20:49:34 +0100

    Added small amout of code to Item.cs and Room.cs

--- The Datamatikers of Tønder/Models/Item.cs
+++ The Datamatikers of Tønder/Models/Item.cs
@@ -2,5 +2,12 @@
 
 public class Item
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
     
+    public Item(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
 }
--- The Datamatikers of Tønder/Models/Room.cs
+++ The Datamatikers of Tønder/Models/Room.cs
@@ -2,5 +2,7 @@
 
 public class Room
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Dictionary<string, Room> Exit { get; set; } = new Dictionary<string, Room>();
 }
