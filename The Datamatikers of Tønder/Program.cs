1c3c116a50623270701a951433d22ade9717e4e1
Alexander <Alexander.Friedrichsen7@gmail.com>
2025-01-31 20:32:35 +0100

    remade the some of the code in Program.cs and connectede the rooms and added items to the rooms

--- The Datamatikers of Tønder/Program.cs
+++ The Datamatikers of Tønder/Program.cs
@@ -10,42 +10,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        //rooms
-        var undervisningslokale = new Room("UnderVisningsLokale", "her bliver du undervist i forskellige emner inde for programmering.");
-        //npc
-        var  erik = new NPC("Erik", "Din underviser", "Hej med dig, jeg er din underviser Erik.");
-        //container
+        //Rooms
+        var undervisningslokale = new Room("Undervisningslokale", "Her bliver du undervist i forskellige emner inden for programmering.");
+        var kantinelokale = new Room("Kantine", "Her kan du købe mad og drikke.");
+        var gangen = new Room("Gangen", "Her kan du gå til de forskellige lokaler.");
+        var toilet = new Room("Toilet", "Her kan du gå på toilettet.");
+        var kontor = new Room("Kontor", "Her kan du snakke med din underviser.");
+        var græskar = new Room("Græskar", "Græskaret er et lille lyd-isoleret rum hvor man kan lave opgaver eller hygge med andre.");
+        var kælderen = new Room("Kælderen", "Kælderen er et mørkt og uhyggeligt sted");
+        var cykelskur = new Room("Cykelskur", "Her kan du parkere din cykel.");
+        
+        
+        //Map
+        //underVisningsLokale
+        undervisningslokale.Exits["North"] = toilet;
+        undervisningslokale.Exits["South"] = gangen;
+        undervisningslokale.Exits["East"] = græskar;
+        undervisningslokale.Exits["West"] = kontor;
+        //gangen
+        gangen.Exits["North"] = undervisningslokale;
+        gangen.Exits["South"] = cykelskur;
+        gangen.Exits["East"] = kantinelokale; 
+        gangen.Exits["Down"] = kælderen;
+        //cykelskur
+        cykelskur.Exits["North"] = gangen;
+        cykelskur.Exits["South"] = kantinelokale;
+        //toilet
+        toilet.Exits["South"] = undervisningslokale;
+        //kontor
+        kontor.Exits["East"] = undervisningslokale;
+        //græskar
+        græskar.Exits["West"] = undervisningslokale;
+        //kantinelokale
+        kantinelokale.Exits["West"] = gangen;
+        kantinelokale.Exits["North"] = cykelskur;
+        //kælderen
+        kælderen.Exits["Up"] = gangen;
+        
+        //items
+        var blyant = new Item("Blyant", "En blyant til at skrive med.");
+        var vrheadset = new Item("VR-headset", "Et VR-headset til at lave VR-programmering.");
+        var computer = new Item("Computer", "En computer til at programmere på.");
+        var papir = new Item("Papir", "Papir til at skrive noter på.");
+        var banan = new Item("Banan", "En banan til at spise.");
+        
+        undervisningslokale.Items.Add(computer);
+        undervisningslokale.Items.Add(vrheadset);
+        kantinelokale.Items.Add(banan);
+        kontor.Items.Add(papir);
+        
+        
+        //beholder i undervisningslokale
         var taske = new Containers("Taske", "En taske til at opbevare dine ting i.", isLocked: false, keyName: "TaskeNøgle");
-        taske.Items.Add(new Item("Blyant", "En blyant til at skrive med."));
-        //enemy
-        undervisningslokale.Enemies.Add(new GymnasieElev());
-        undervisningslokale.NPCs.Add(erik);
+        taske.Items.Add(blyant);
         undervisningslokale.Containers.Add(taske);
         
         
         
-        //rooms
-        var kantine = new Room("Kantine", "her kan du købe mad og drikke.");
-        //npc
-        var  kantineDame = new NPC("KantineDame", "Damen i kantinen", "Hej med dig, jeg er damen i kantinen.");
-        //Enemy
-        var gymnasiestuderende = new Enemy("Gymnasiestuderende", 10, 2, 5);
-        //container
-       
-        
-        kantine.NPCs.Add(kantineDame);
-        kantine.Enemies.Add(gymnasiestuderende);
+        //enemy
+        kantinelokale.Enemies.Add(new GymnasieElev());
+        kælderen.Enemies.Add(new Pedel());
         
         
         
-        undervisningslokale.Exits["North"] = kantine;
-        kantine.Exits["South"] = undervisningslokale;
-        
-        var player = new Player("Hero", undervisningslokale);
+        var player = new Player("Hero", cykelskur);
         var gameEngine = new GameEngine(player);
         
-        Console.WriteLine("Welcome to the game!");
-        Console.WriteLine("Type 'look' to inspect the room, or 'go [direction]' to move.");
+        Console.WriteLine("Velkommen til første dag som datamatik studerende i Tønder!");
+        Console.WriteLine("skriv 'look' for at inspisere et lokale, eller 'go [direction]' for at bevæge dig.");
 
         while (true)
         {
